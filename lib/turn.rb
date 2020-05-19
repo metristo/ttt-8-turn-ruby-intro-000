c6ba671e940e1d680d1f8cd8e098a05e15c2b99f
Michael G Rivera <rivera.mgr@gmail.com>
2020-05-19 12:19:52 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,7 @@ def turn(board)
   string = gets.strip
   index = input_to_index(string)
   if valid_move?(board, index) == true
-    move(board, index, token = "x")
+    move(board, index, token)
   elsif valid_move?(board, index) == false
     turn(board)
   end
