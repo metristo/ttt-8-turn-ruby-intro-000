538c30e32b42d869e890ffec48a3699af6a9637a
Michael G Rivera <rivera.mgr@gmail.com>
2020-05-19 11:46:49 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,36 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == true
+    false
+  elsif index.between?(0, 8)
+    true
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || "O"
+    true
+  end
+end
+
+def input_to_index(string)
+  input = string.to_i
+  index = input - 1
+end
+
+def move(board, index, token = "X")
+  board[index] = token
+end
